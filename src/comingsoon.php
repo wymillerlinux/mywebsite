75eabc4dffde4781e6ce35db2093e9c3e4d86063
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-01-08 13:30:24 -0500

    added a blog header item

--- src/comingsoon.php
+++ src/comingsoon.php
@@ -2,24 +2,24 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Wyatt J. Miller - About</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="col-xs-12">
-        <div class="header">
-            <div class="header-title">
-                <h1>Wyatt J. Miller</h1>
+    <?php require "header.php" ?>
+                <div class="col-xs-12">
+                    <div class="main-section-about">
+                        <div class="main-background">
+                            <h2>Coming soon!</h2><br>
+                            <h2>My blog is coming in the near future.</h2>
+                            <h2 class="main-background-text">If you have any questions regarding the blog, <a href="mailto:wjmiller2016@gmail.com">email</a> me!</h2>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <nav class="nav nav-pills nav-justified">
-                <a class="nav-item nav-link" href="/">Home</a>
-                <a class="nav-item nav-link" href="about.php">About</a>
-                <a class="nav-item nav-link" href="comingsoon.php">Blog</a>
-                <a class="nav-item nav-link" href="https://github.com/wymillerlinux?tab=repositories" target="_blank">Projects</a>
-                <a class="nav-item nav-link" href="mailto:wjmiller2016@gmail.com">Contact</a>
-            </nav>
         </div>
-    </div>
+    <?php require "footer.php" ?>
 </body>
