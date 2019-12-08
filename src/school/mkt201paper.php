2c357d2c54754163bd60a2b37c5c4bbf3fd6d4ee
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-12-08 14:05:30 -0500

    added school path, added school functions

---
+++ src/school/mkt201paper.php
@@ -0,0 +1,4 @@
+<?
+$url = "https://wyattjmiller.com/comingsoon.php";
+header("Location: " . $url);
+?>
---
+++ src/school/mkt201pres.php
@@ -0,0 +1,4 @@
+<?
+$url = "https://docs.google.com/presentation/d/1Aa3veC98tlwjQLTsJ7waoCtq7DooP7zGvhijK0W9fwA/";
+header("Location: " . $url);
+?>
