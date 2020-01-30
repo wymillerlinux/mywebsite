76281c3b60750b3b6b9f50d062704e82a5d38af7
Wyatt Miller <wjmiller2016@gmail.com>
2020-01-30 10:02:53 -0500

    typo

--- src/school/com111fact.php
+++ src/school/com111fact.php
@@ -1,4 +1,4 @@
 <?
-$url = "https://docs.google.com/presentation/d/1b3IyHs2Y3FneB_PDI9reP5hvrv0tUoAKA52Xh_ttu78/s";
+$url = "https://docs.google.com/presentation/d/1b3IyHs2Y3FneB_PDI9reP5hvrv0tUoAKA52Xh_ttu78/";
 header("Location: " . $url);
 ?>
