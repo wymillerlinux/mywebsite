679269f9e23d4e249fc6fbe80face56f995d13b1
Wyatt J. Miller <wjmiller2016@gmail.com>
2020-02-20 09:07:33 -0500

    added php tags
    I'm an idiot

--- src/school/eco202pres.php
+++ src/school/eco202pres.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $url = "https://docs.google.com/presentation/d/1pLF2JgItDBqmTCm6upLffkmXI6B-WL4SzkMJ_xvWARI/edit";
 header("Location: " . $url);
 ?>
--- src/school/mkt201pres.php
+++ src/school/mkt201pres.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $url = "https://docs.google.com/presentation/d/1Aa3veC98tlwjQLTsJ7waoCtq7DooP7zGvhijK0W9fwA/";
 header("Location: " . $url);
 ?>
